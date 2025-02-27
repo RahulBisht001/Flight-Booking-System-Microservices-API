5f92eb1cd82f2148d05b4ed87b3903df03da5377
RahulB <rahulbisht1012@gmail.com>
2025-02-27 16:48:25 +0530

    Fix: Improve error propagation by removing try-catch in crud-repository
    we removed the try catch block from crud repository because technically
    we are handling the errors in the services layer very efficiently and
    controller will just pass on the response or error to the client.

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -11,52 +11,32 @@ class CrudRepository {
 	}
 
 	async destroy(data) {
-		try {
-			const response = await this.model.destroy({
-				where: {
-					id: data,
-				},
-			});
-			return response;
-		} catch (error) {
-			Logger.error("Something went wrong in the `crud repository` : destroy");
-			throw error;
-		}
+		const response = await this.model.destroy({
+			where: {
+				id: data,
+			},
+		});
+		return response;
 	}
 
 	async get(data) {
-		try {
-			const response = await this.model.findByPk(data);
-			return response;
-		} catch (error) {
-			Logger.error("Something went wrong in the `crud repository` : get");
-			throw error;
-		}
+		const response = await this.model.findByPk(data);
+		return response;
 	}
 
 	async getAll() {
-		try {
-			const response = await this.model.findAll();
-			return response;
-		} catch (error) {
-			Logger.error("Something went wrong in the `crud repository` : getAll");
-			throw error;
-		}
+		const response = await this.model.findAll();
+		return response;
 	}
 
 	async update(id, data) {
 		// data is the object that needs to be updated
-		try {
-			const response = await this.model.update(data, {
-				where: {
-					id: id,
-				},
-			});
-			return response;
-		} catch (error) {
-			Logger.error("Something went wrong in the `crud repository` : update");
-			throw error;
-		}
+		const response = await this.model.update(data, {
+			where: {
+				id: id,
+			},
+		});
+		return response;
 	}
 }
 
